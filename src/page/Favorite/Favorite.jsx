45774aede9768275484249754833e27cc80de7ae
JuliaTarasenko00 <juliatarase17@gmail.com>
2024-01-03 16:10:41 +0200

    fix favorite page

--- src/page/Favorite/Favorite.jsx
+++ src/page/Favorite/Favorite.jsx
@@ -3,6 +3,7 @@ import { Section } from 'components/TeachersMarkup/TeachersMarkup.styled';
 import { database } from 'config/firebase-config';
 import { Container } from 'globalStyles';
 import { useFavorite } from 'helpers/favorite';
+import { Title } from './Favorite.styled';
 
 const FavoritePage = () => {
   const favorite = useFavorite(database);
@@ -11,7 +12,9 @@ const FavoritePage = () => {
     <Section>
       <Container>
         <TeachersMarkup item={favorite} />
-        {favorite.length === 0 && <p>Not Found</p>}
+        {favorite.length === 0 && (
+          <Title>You haven't added teachers to your favorites yet.</Title>
+        )}
       </Container>
     </Section>
   );
